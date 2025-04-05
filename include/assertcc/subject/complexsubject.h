ce139386b2a1b5e0e372c67432812256b6b7ab2e
Sean Nash <sean.c.nash@gmail.com>
2025-04-05 14:09:28 -0400

    Simplify ComplexSubject

--- include/assertcc/subject/complexsubject.h
+++ include/assertcc/subject/complexsubject.h
@@ -10,22 +10,22 @@ namespace assertcc::subject {
 
 template <typename T>
 class ComplexSubject
-    : public virtual Base<std::complex<T>>,
-      public proposition::IsEqualToPropositions<ComplexSubject<T>, std::complex<T>>,
-      public proposition::IsInPropositions<ComplexSubject<T>, std::complex<T>> {
-  const std::complex<T> d_value;
+    : public virtual Base<T>,
+      public proposition::IsEqualToPropositions<ComplexSubject<T>, T>,
+      public proposition::IsInPropositions<ComplexSubject<T>, T> {
+  const T d_value;
 
  protected:
-  const std::complex<T>* getObject() const override { return &d_value; }
-  const std::string getObjectAsString(const std::complex<T>& other) const override {
+  const T* getObject() const override { return &d_value; }
+  const std::string getObjectAsString(const T& other) const override {
     std::stringstream ss;
     ss << other.real() << " + " << other.imag() << "i";
     return ss.str();
   }
 
  public:
-  ComplexSubject(const bool failOnError, const char* file, int line, const std::complex<T> v)
-      : Base<std::complex<T>>(failOnError, file, line), d_value(v) {}
+  ComplexSubject(const bool failOnError, const char* file, int line, const T v)
+      : Base<T>(failOnError, file, line), d_value(v) {}
 
 };
 
