416d9cbd1e12c52fd701c304536a17cfa43a4677
Sean Nash <sean.c.nash@gmail.com>
2025-04-21 14:14:06 -0400

    Include missing file

---
+++ include/assertcc/proposition/containspropositions.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <assertcc/subject/base.h>
+#include <assertcc/util/failmessage.h>
+#include <memory.h>
+#include <vector>
+#include <set>
+
+namespace assertcc::proposition {
+
+template <typename T, typename U>
+class ContainsPropositions : public virtual subject::Base<U> {
+
+ public:
+  template <typename Value>
+  T& contains(const Value& value) {
+    if (this->getObject()->find(value) == this->getObject()->end()) {
+      util::FailMessage::create()
+          .failOnError(this->getFailOnError())
+          .file(this->getFile())
+          .line(this->getLine())
+          .fact("contains value", value)
+          .fact("Got", this->getObjectAsString());
+    }
+    return *dynamic_cast<T*>(this);
+  }
+
+  template <typename Value>
+  T& doesNotContain(const Value& value) {
+    auto iter = this->getObject()->find(value);
+    if (iter != this->getObject()->end()) {
+      util::FailMessage::create()
+          .failOnError(this->getFailOnError())
+          .file(this->getFile())
+          .line(this->getLine())
+          .fact("to not contain the value ", value)
+          .fact("Got", this->getObjectAsString())
+          .build();
+    }
+    return *dynamic_cast<T*>(this);
+  }
+
+};
+
+}  // namespace assertcc::proposition 
